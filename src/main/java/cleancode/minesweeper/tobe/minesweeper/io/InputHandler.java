e30774caaa0be626dbb643630283da2865bb5e62
jaewon926 <jjojjo0926@gmail.com>
2024-10-08 18:42:27 +0900

    "refactoring"

---
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/io/InputHandler.java
@@ -0,0 +1,9 @@
+package cleancode.minesweeper.tobe.minesweeper.io;
+
+import cleancode.minesweeper.tobe.minesweeper.board.position.CellPosition;
+import cleancode.minesweeper.tobe.minesweeper.user.UserAction;
+
+public interface InputHandler {
+    CellPosition getCellPositionFromUser();
+    UserAction getUserActionFromUser();
+}
